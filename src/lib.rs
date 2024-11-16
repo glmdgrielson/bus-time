afb80c7e7691a64863769ccda4f159ba4a1a237f
glmdgrielson <zapperchamp1@gmail.com>
2024-11-16 17:37:45 -0500

    Initialize repository
    Adding the initial attempts and an editorconfig for good measure.

---
+++ src/lib.rs
@@ -0,0 +1,16 @@
+//! A library for measuring time in the style of Desert Bus's shift clocks.
+
+pub fn add(left: usize, right: usize) -> usize {
+    left + right
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
